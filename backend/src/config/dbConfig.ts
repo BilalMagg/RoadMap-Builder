20023d9372d9bb3557d7947cac3c32a664d44dca
Bilal <gygcjg55@gmail.com>
2025-12-21 17:10:11 +0100

    Final Backend test version

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -2,11 +2,10 @@ import { IDatabaseConfig } from "./database.interface";
 import { DatabaseFactory } from "./database.factory";
 
 
-const env = process.env.NODE_ENV || "test" ;
+const env = process.env.NODE_ENV || "dev" ;
 const DEV = process.env.ENV_DEV;
 const TEST = process.env.ENV_TEST;
 const PROD = process.env.ENV_PROD;
-console.log("Env choice : ",env);
 
 
 const linkDataSource = () : IDatabaseConfig => {
